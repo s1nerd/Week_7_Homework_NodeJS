b5a91f92abf48be4772cae1ec23692282774df4e
s1nerd <irpanmln@gmail.com>
2023-09-22 15:30:47 +0700

    Perubahan File

--- soal1/soal1.js
+++ soal1/soal1.js
@@ -1,26 +1,90 @@
-const geometry = require("./geometry");
+// soal1.js
+const fs = require("fs");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// fungsi dari modul geometry.js
-const sisiPersegi = 5;
-const panjangPersegiPanjang = 6;
-const lebarPersegiPanjang = 4;
+const persegipanjang = require("./persegipanjang");
+const persegi = require("./persegi");
 
-const luasPersegi = geometry.hitungLuasPersegi(sisiPersegi);
-const kelilingPersegi = geometry.hitungKelilingPersegi(sisiPersegi);
-const luasPersegiPanjang = geometry.hitungLuasPersegiPanjang(
-  panjangPersegiPanjang,
-  lebarPersegiPanjang
-);
-const kelilingPersegiPanjang = geometry.hitungKelilingPersegiPanjang(
-  panjangPersegiPanjang,
-  lebarPersegiPanjang
-);
+class Rumus {
+  constructor() {
+    this.start();
+  }
 
-console.log(`Luas Persegi dengan sisi ${sisiPersegi}: ${luasPersegi}`);
-console.log(`Keliling Persegi dengan sisi ${sisiPersegi}: ${kelilingPersegi}`);
-console.log(
-  `Luas Persegi Panjang dengan panjang ${panjangPersegiPanjang} dan lebar ${lebarPersegiPanjang}: ${luasPersegiPanjang}`
-);
-console.log(
-  `Keliling Persegi Panjang dengan panjang ${panjangPersegiPanjang} dan lebar ${lebarPersegiPanjang}: ${kelilingPersegiPanjang}`
-);
+  start() {
+    rl.question(
+      "Pilih bangun datar (1. Persegi Panjang, 2. Persegi): ",
+      (choice) => {
+        if (choice === "1") {
+          this.hitungPersegiPanjang();
+        } else if (choice === "2") {
+          this.hitungPersegi();
+        } else {
+          console.log("Pilihan tidak valid.");
+          rl.close();
+        }
+      }
+    );
+  }
+
+  hitungPersegiPanjang() {
+    rl.question("Masukkan panjang: ", (panjang) => {
+      rl.question("Masukkan lebar: ", (lebar) => {
+        const luas = persegipanjang.hitungLuas(panjang, lebar);
+        const keliling = persegipanjang.hitungKeliling(panjang, lebar);
+
+        // Menyusun hasil perhitungan ke dalam format HTML
+        const hasilHTML = `
+          <html>
+          <head>
+            <title>Hasil Perhitungan</title>
+          </head>
+          <body>
+            <h1>Hasil Perhitungan</h1>
+            <p>Luas Persegi Panjang: ${luas}</p>
+            <p>Keliling Persegi Panjang: ${keliling}</p>
+          </body>
+          </html>
+        `;
+
+        // Menyimpan hasil perhitungan dalam sebuah file HTML
+        fs.writeFileSync("hasil.html", hasilHTML);
+
+        console.log("Hasil perhitungan telah disimpan dalam hasil.html");
+        rl.close();
+      });
+    });
+  }
+
+  hitungPersegi() {
+    rl.question("Masukkan panjang sisi: ", (sisi) => {
+      const luas = persegi.hitungLuas(sisi);
+      const keliling = persegi.hitungKeliling(sisi);
+
+      // Menyusun hasil perhitungan ke dalam format HTML
+      const hasilHTML = `
+        <html>
+        <head>
+          <title>Hasil Perhitungan</title>
+        </head>
+        <body>
+          <h1>Hasil Perhitungan</h1>
+          <p>Luas Persegi: ${luas}</p>
+          <p>Keliling Persegi: ${keliling}</p>
+        </body>
+        </html>
+      `;
+
+      // Menyimpan hasil perhitungan dalam sebuah file HTML
+      fs.writeFileSync("hasil.html", hasilHTML);
+
+      console.log("Hasil perhitungan telah disimpan dalam hasil.html");
+      rl.close();
+    });
+  }
+}
+
+new Rumus();
