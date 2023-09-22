fc048b316ca82c96bf998f1824ae71e539c386af
s1nerd <irpanmln@gmail.com>
2023-09-22 15:36:06 +0700

    Tambahan file hasil.html, persegi.js, persegipanjang.js dan soal1.js

---
+++ soal1/persegi.js
@@ -0,0 +1,9 @@
+// persegi.js
+module.exports = {
+  hitungLuas: function (sisi) {
+    return sisi * sisi;
+  },
+  hitungKeliling: function (sisi) {
+    return 4 * sisi;
+  },
+};
---
+++ soal1/persegipanjang.js
@@ -0,0 +1,9 @@
+// persegipanjang.js
+module.exports = {
+  hitungLuas: function (panjang, lebar) {
+    return panjang * lebar;
+  },
+  hitungKeliling: function (panjang, lebar) {
+    return 2 * (panjang + lebar);
+  },
+};
